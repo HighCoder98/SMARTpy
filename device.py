3cd973a3e6740bcfc80cad01be22a16dc2ee7e4e
HighCoder98 <highcoder@outlook.de>
2016-08-18 16:19:40 +0200

    Removed outcommented constructor

--- device.py
+++ device.py
@@ -19,10 +19,3 @@ class SMARTDevice(object):
     temp = None
     health_status = None
     power_on_hours = None
-   
-    # def __init__(self, name, serial, temp, max_temp, min_temp):
-    #     self.name = name
-    #     self.serial = serial
-    #     self.temp = temp
-    #     self.max_temp = max_temp
-    #     self.min_temp = min_temp
